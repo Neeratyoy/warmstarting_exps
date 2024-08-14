f17c4f6a65b9f3a9f30e7cbee8be48201665876a
neeratyoy <neeratyoy@gmail.com>
2024-08-14 04:12:30 +0200

    Adding simple template

--- meta/scripts/simple_run_wikitext.sh
+++ meta/scripts/simple_run_wikitext.sh
@@ -1,5 +1,5 @@
 python examples/run_configs.py \
-    --data_config_path /work/dlclarge1/mallik-scaling/scaling_exps/configs/wikitext_data_handler.yaml \
-    --train_config_path /work/dlclarge1/mallik-scaling/scaling_exps/configs/train_${1}.yaml \
-    --output_dir /work/dlclarge1/mallik-scaling/scaling_exps/testing/${1}
+    --data_config_path /work/dlclarge1/mallik-warmstarting/warmstarting_exps/configs/wikitext_data_handler.yaml \
+    --train_config_path /work/dlclarge1/mallik-warmstarting/warmstarting_exps/configs/train_template.yaml \
+    --output_dir /work/dlclarge1/mallik-warmstarting/scale_and_warmstart/results/test_run/${1}
 # end of file
