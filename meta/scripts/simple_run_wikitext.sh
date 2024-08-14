9192f079f2d2655010bb0c872dfb4ac2c980cf9f
neeratyoy <neeratyoy@gmail.com>
2024-08-14 04:43:52 +0200

    Adding simple run

--- meta/scripts/simple_run_wikitext.sh
+++ meta/scripts/simple_run_wikitext.sh
@@ -1,5 +1,6 @@
+cd /work/dlclarge1/mallik-warmstarting/scale_and_warmstart/
 python examples/run_configs.py \
     --data_config_path /work/dlclarge1/mallik-warmstarting/warmstarting_exps/configs/wikitext_data_handler.yaml \
     --train_config_path /work/dlclarge1/mallik-warmstarting/warmstarting_exps/configs/train_template.yaml \
-    --output_dir /work/dlclarge1/mallik-warmstarting/scale_and_warmstart/results/test_run/${1}
+    --output_dir /work/dlclarge1/mallik-warmstarting/scale_and_warmstart/results/test_run
 # end of file
